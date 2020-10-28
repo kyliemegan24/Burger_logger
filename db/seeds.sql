dc518f118ab970afc10dc6f7759e2c9770bf36e6
Kylie S <kyliemegan24@gmail.com>
2020-10-28 17:48:41 -0500

    Set up the database using mysql and set up the connection. Also pushing up general folder structure/skeleton files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured) VALUES 
+  ('Da Burger', 0),
+  ('The Wahlger', 0),
+  ('The Bully', 0),
+  ('The Podey Nodey', 0);
+  -- ('Big Meatloaf', 0),
+  -- ('Double Stacks', 0),
+  -- ('Mushroom Paradize', 1),
+  -- ('Spicy Tofu', 1),
+  -- ('Garden Veggie', 1),
+  -- ('Beanlicious', 0);
+
