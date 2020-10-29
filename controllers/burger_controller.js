377b16c436f1eae659a72b10387a57fe5d44032d
Kylie S <kyliemegan24@gmail.com>
2020-10-28 19:05:06 -0500

    Filled out burger_controller.js file

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,23 +1,49 @@
 const express = require('express');
 
 //Import the model to use its database functions
-const burger = require('../models/burger');
+const burger = require('../models/burger.js');
 
-const app = express();
+const router = express.Router();
 
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+        var hbsObject = {
+          burgers: data
+        };
+        //console.log(hbsObject);
+        res.render("index", hbsObject);
+      });
     
 
 });
 
-app.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function(req, res) {
+    burger.create(
+      ["burger_name", "devoured"], 
+      [req.body.burger_name, req.body.devoured],
+       function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+      });
     
 
 });
 
 
-app.put("/api/burgers/:id", function(req, res) {
-    
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  
 
 });
 
